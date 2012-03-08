308d69a2a1db84b009953d45b68efcb1c57ae291
Steffen Tröster <stetro.freaky@googlemail.com>
2012-03-08 17:11:19 +0100

    Normalenvektoren werden exportiert

--- src/main/java/amcgala/framework/shape/util/PLYPolygonParser.java
+++ src/main/java/amcgala/framework/shape/util/PLYPolygonParser.java
@@ -32,96 +32,107 @@ import java.util.Scanner;
  * Statischer PLY File Parser zum laden von Polygonen. Export von komplexen
  * Objekten aus Blender moeglich (siehe exsample Paket) <br/>
  * <br/>
- *
+ * 
  * @author Steffen Tröster
  */
 public class PLYPolygonParser {
 
-    private static final Logger log = LoggerFactory
-            .getLogger(PLYPolygonParser.class);
+	private static final Logger log = LoggerFactory
+			.getLogger(PLYPolygonParser.class);
 
-    /**
-     * Parst .ply File und gibt Polygone zurueck<br/>
-     * <br/>
-     *
-     * @param filePath Pfad zur .ply Datei
-     * @param scale    Skalierung des Objektes (zB 100)
-     * @return ArrayList von allen Polygonen
-     * @throws IOException
-     */
-    public static List<Polygon> parseAsPolygonList(String filePath, double scale)
-            throws Exception {
-        // Stream öffnen (Exception!)
-        InputStream inputStream = new FileInputStream(filePath);
-        return parseAsPolygonList(inputStream, scale);
-    }
+	/**
+	 * Parst .ply File und gibt Polygone zurueck<br/>
+	 * <br/>
+	 * 
+	 * @param filePath
+	 *            Pfad zur .ply Datei
+	 * @param scale
+	 *            Skalierung des Objektes (zB 100)
+	 * @return ArrayList von allen Polygonen
+	 * @throws IOException
+	 */
+	public static List<Polygon> parseAsPolygonList(String filePath, double scale)
+			throws Exception {
+		// Stream öffnen (Exception!)
+		InputStream inputStream = new FileInputStream(filePath);
+		return parseAsPolygonList(inputStream, scale);
+	}
 
-    /**
-     * Parst .ply File und gibt Polygone zurueck<br/>
-     * <br/>
-     *
-     * @param inputStream to File
-     * @param scale       Skalierung des Objektes (zB 100)
-     * @return ArrayList von allen Polygonen
-     * @throws IOException
-     */
-    public static List<Polygon> parseAsPolygonList(InputStream inputStream,
-                                                   double scale) throws Exception {
-        // resultierende Polygone
-        ArrayList<Polygon> polygons = new ArrayList<Polygon>();
-        // Koordinaten Anzahl
-        int vertexCount = 0;
-        // Polygone Anzahl
-        int faceCount = 0;
-        // Koordinaten
-        ArrayList<Vector3d> vertices = new ArrayList<Vector3d>();
-        // Stream in Scanner legen
-        final Scanner scanner = new Scanner(inputStream).useLocale(Locale.US);
+	/**
+	 * Parst .ply File und gibt Polygone zurueck<br/>
+	 * <br/>
+	 * 
+	 * @param inputStream
+	 *            to File
+	 * @param scale
+	 *            Skalierung des Objektes (zB 100)
+	 * @return ArrayList von allen Polygonen
+	 * @throws IOException
+	 */
+	public static List<Polygon> parseAsPolygonList(InputStream inputStream,
+			double scale) throws Exception {
+		// resultierende Polygone
+		ArrayList<Polygon> polygons = new ArrayList<Polygon>();
+		// resultierende Normalen
+		ArrayList<Vector3d> normals = new ArrayList<Vector3d>();
+		// Koordinaten Anzahl
+		int vertexCount = 0;
+		// Polygone Anzahl
+		int faceCount = 0;
+		// Koordinaten
+		ArrayList<Vector3d> vertices = new ArrayList<Vector3d>();
+		// Stream in Scanner legen
+		final Scanner scanner = new Scanner(inputStream).useLocale(Locale.US);
 
-        // Durchlauf des Dateiheaders
-        while (scanner.hasNext() && scanner.hasNextLine()) {
-            String currentLine = scanner.nextLine();
-            // Lade Anzahl von Koordinaten
-            if (currentLine.matches("element vertex [0-9]{1,5}")) {
-                vertexCount = Integer
-                        .parseInt(currentLine.substring(15).trim());
-            }
-            // Lade Anzahl von Polygonen
-            if (currentLine.matches("element face [0-9]{1,5}")) {
-                faceCount = Integer.parseInt(currentLine.substring(12).trim());
-            }
-            // Next step wenn Dateiheader gelesen
-            if (currentLine.matches("end_header")) {
-                break;
-            }
-        }
-        // Lade alle Koordinaten
-        for (int i = 0; i < vertexCount; i++) {
-            vertices.add(new Vector3d(scanner.nextDouble() * scale, scanner
-                    .nextDouble() * scale, scanner.nextDouble() * scale));
-            scanner.nextLine();
-        }
+		// Durchlauf des Dateiheaders
+		while (scanner.hasNext() && scanner.hasNextLine()) {
+			String currentLine = scanner.nextLine();
+			// Lade Anzahl von Koordinaten
+			if (currentLine.matches("element vertex [0-9]{1,5}")) {
+				vertexCount = Integer
+						.parseInt(currentLine.substring(15).trim());
+			}
+			// Lade Anzahl von Polygonen
+			if (currentLine.matches("element face [0-9]{1,5}")) {
+				faceCount = Integer.parseInt(currentLine.substring(12).trim());
+			}
+			// Next step wenn Dateiheader gelesen
+			if (currentLine.matches("end_header")) {
+				break;
+			}
+		}
+		// Lade alle Koordinaten
+		for (int i = 0; i < vertexCount; i++) {
+			vertices.add(new Vector3d(scanner.nextDouble() * scale, scanner
+					.nextDouble() * scale, scanner.nextDouble() * scale));
+			normals.add(new Vector3d(scanner.nextDouble() * scale, scanner
+					.nextDouble() * scale, scanner.nextDouble() * scale));
+			scanner.nextLine();
+		}
 
-        // Lade alle Polygone
-        for (int i = 0; i < faceCount; i++) {
-            int currentFace = scanner.nextInt();
-            if (currentFace == 4) {
-                polygons.add(new Polygon(vertices.get(scanner.nextInt()),
-                        vertices.get(scanner.nextInt()), vertices.get(scanner
-                        .nextInt()), vertices.get(scanner.nextInt())));
-            } else if (currentFace == 3) {
-                polygons.add(new Polygon(vertices.get(scanner.nextInt()),
-                        vertices.get(scanner.nextInt()), vertices.get(scanner
-                        .nextInt())));
-            } else {
-                throw new IOException();
+		// Lade alle Polygone und setze Normalen
+		for (int i = 0; i < faceCount; i++) {
+			int currentFace = scanner.nextInt();
+			if (currentFace == 4) {
+				int index = scanner.nextInt();
+				polygons.add(new Polygon(vertices.get(index), vertices
+						.get(scanner.nextInt()),
+						vertices.get(scanner.nextInt()), vertices.get(scanner
+								.nextInt()), normals.get(index)));
+			} else if (currentFace == 3) {
+				int index = scanner.nextInt();
+				polygons.add(new Polygon(vertices.get(index), vertices
+						.get(scanner.nextInt()),
+						vertices.get(scanner.nextInt()), normals.get(index)));
+			} else {
+				throw new IOException();
 
-            }
-        }
+			}
+		}
 
-        log.info("loaded " + vertexCount + " vertexes and " + faceCount
-                + " faces");
-        return polygons;
+		log.info("loaded " + vertexCount + " vertexes and " + faceCount
+				+ " faces");
+		return polygons;
 
-    }
+	}
 }
