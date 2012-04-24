a672ae3f66557b309a1edbae3b5f1193d3952814
Steffen Tröster <stetro.freaky@googlemail.com>
2012-04-24 09:25:02 +0200

    Letter Example

---
+++ src/main/java/amcgala/example/letter/LetterMain.java
@@ -0,0 +1,23 @@
+package amcgala.example.letter;
+
+import amcgala.Framework;
+import amcgala.framework.shape2d.Text;
+
+
+public class LetterMain extends Framework {
+
+	public LetterMain(int width, int height) {
+		super(width, height);
+	}
+
+	public static void main(String[] args) {
+		new LetterMain(500, 250).start();
+
+	}
+
+	@Override
+	public void initGraph() {
+		add(new Text("amCGAla ! Test", -100, 0));
+
+	}
+}
