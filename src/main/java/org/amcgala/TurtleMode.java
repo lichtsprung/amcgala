8c81afd74eb719b20307c89ef3dd8b5616cbb438
Robert Giacinto <robert.giacinto@gmail.com>
2012-10-06 19:29:43 +0200

    turtlemode javadoc

--- src/main/java/org/amcgala/TurtleMode.java
+++ src/main/java/org/amcgala/TurtleMode.java
@@ -14,13 +14,20 @@ import static java.lang.Math.*;
  * @since 2.0
  */
 public abstract class TurtleMode {
+    /**
+     * Die Standardbreite des Turtlefensters.
+     */
     protected final static int WIDTH = 800;
+    /**
+     * Die Standardhöhe des Turtlefensters.
+     */
     protected final static int HEIGHT = 600;
     private final Scene scene = new Scene("turtle");
     private Vector3d heading = Vector3d.UNIT_X;
     private Vector3d position = Vector3d.ZERO;
     private double headingAngle;
     private boolean up;
+
 
     public TurtleMode() {
         turtleCommands();
@@ -37,26 +44,46 @@ public abstract class TurtleMode {
         position = new Vector3d(x, y, -1);
     }
 
+    /**
+     * Nimmt den Zeichenstift der Turtle von der Zeichenfläche und stoppt das Zeichnen.
+     * Nach dem Aufruf dieser Methode kann die Turtle bewegt werden, ohne Änderungen an der Zeichnung vorzunehmen.
+     */
     protected void up(){
         up = true;
     }
 
+    /**
+     * Setzt den Zeichenstift der Turtle wieder auf der Zeichenfläche ab und beginnt das Zeichnen, sobald die Turtle nach
+     * dem Aufruf dieser Methode wieder bewegt wird.
+     */
     protected void down(){
         up = false;
     }
 
+    /**
+     * Dreht die Turtle entgegen den Uhrzeigersinn um den Winkel, der als Parameter übergeben wird.
+     * @param angle der Winkel in Grad
+     */
     protected void turnLeft(double angle) {
         headingAngle += angle;
         heading = new Vector3d(cos(toRadians(headingAngle)), sin(toRadians(headingAngle)), -1);
         heading.normalize();
     }
 
+    /**
+     * Dreht die Turtle im Uhrzeigersinn um den Winkel, der als Parameter übergeben wird.
+     * @param angle der Winkel in Grad
+     */
     protected void turnRight(double angle) {
         headingAngle -= angle;
         heading = new Vector3d(cos(toRadians(headingAngle)), sin(toRadians(headingAngle)), -1);
         heading.normalize();
     }
 
+    /**
+     * Bewegt die Turtle um eine bestimmte Länge in Blickrichtung der Turtle.
+     * @param length die Länge des Schritts
+     */
     protected void move(double length) {
         checkArgument(length > 0, "Schrittlänge kann nur positiv sein!");
 
