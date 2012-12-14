670d599bb4f8ef90f8fce3e10d7e6487b8caa64a
Robert Giacinto <robert.giacinto@gmail.com>
2012-12-14 15:00:59 +0100

    kugel

--- src/main/java/org/amcgala/framework/shape/shape3d/Sphere.java
+++ src/main/java/org/amcgala/framework/shape/shape3d/Sphere.java
@@ -26,20 +26,34 @@ public class Sphere extends AbstractShape {
     public boolean hit(Ray ray, ShadingInfo shadingInfo) {
         shadingInfo.ray = ray;
         shadingInfo.label = getLabel();
+        Vector3d tmp = ray.origin.sub(center);
+        double a = ray.direction.dot(ray.direction);
+        double b = ray.direction.dot(tmp) * 2;
+        double c = tmp.dot(tmp) - radius * radius;
+        double discriminant = b * b - 4.0 * a * c;
 
-        /*
-         * TODO
-         * Schnittpunktberechnung Gerade - Kugel.
-         *
-         * Verwenden Sie die Zusatzinformationen von der Website, um die Berechnung korrekt zu implementieren.
-         * Im Objekt shadingInfo sollten folgende Informationen gespeichert werden:
-         *      - t
-         *      - die Normale
-         *      - der Schnittpunkt
-         *      - die Farbe der Kugel
-         */
+        if (discriminant >= 0) {
+            double e = Math.sqrt(discriminant);
+            double denominator = 2.0 * a;
+            double t = (-b - e) / denominator;
 
-
+            if (t > MathConstants.EPSILON) {
+                shadingInfo.t = t;
+                shadingInfo.normal = tmp.add(ray.direction.times(t)).times(1 / radius);
+                shadingInfo.hitPoint = ray.origin.add(ray.direction.times(t));
+                shadingInfo.color = material.getColor(shadingInfo);
+                return true;
+            } else {
+                t = (-b + e) / denominator;
+                if (t > MathConstants.EPSILON) {
+                    shadingInfo.t = t;
+                    shadingInfo.normal = tmp.add(ray.direction.times(t)).times(1 / radius);
+                    shadingInfo.hitPoint = ray.origin.add(ray.direction.times(t));
+                    shadingInfo.color = material.getColor(shadingInfo);
+                    return true;
+                }
+            }
+        }
         return false;
     }
 
